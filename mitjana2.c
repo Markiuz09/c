1f5409feac27e801578c1b35c6b2e4227eeff923
Markiuz09 <martorfon@alu.edu.gva.es>
2025-10-06 11:30:43 +0200

    Add files via upload

---
+++ mitjana2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main (){
+   int a, b;
+   float mitjana;
+   printf("1r núm?: ");
+   scanf ("%d\n" , &a);
+   printf("2n núm?: ");
+   scanf ("%d\n" , &b);
+   printf("La mitjana és: %d\n", (a+b)/2.0);
+   return;
+   }
---
+++ swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main (){
+   int a, b;
+
+   printf("Intro a: ");
+   scanf ("%d" , &a);
+   printf("Intro b: ");
+   scanf ("%d" , &b);
+   a=aux;
+   a=b;
+   b=aux;
+   printf("Intercanviat a i b es: %d | %d\n", a, b);
+   return;
+   }
