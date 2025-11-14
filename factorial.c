e303aa2c36dec84581269e0290a61e8729c8e6ba
Markiuz09 <martorfon@alu.edu.gva.es>
2025-11-14 12:31:46 +0100

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+ int factorial(int n)
+ {
+    int aux, i;
+    aux=1;
+    for(i=1;i<=n;i++) aux=aux*i;
+    return aux;
+ }
+
+ void main(){
+    int n;
+    printf("Factorial de: ");
+    scanf("%d",&n);
+    printf("%d\n", factorial(n));
+    return;
+
+    }
