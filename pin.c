d8e4610be13ab57ada91e928165384df5a24e3b8
Markiuz09 <martorfon@alu.edu.gva.es>
2025-11-14 12:24:14 +0100

    Refactor PIN input and validation logic

--- pin.c
+++ pin.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 void main(){
-    int pin;
-    printf("PIN??: ");
-    scanf("%d", &pin);
-    if(pin==1234) printf ("OK!!");
-    else printf("Incorrecte!!");
+int resposta;
+printf("Pin?: ");
+scanf("%d", &resposta);
+if(resposta==1234)
+    printf("Ok!\n");
+else printf("Incorrecte!\n");
 return;
-]
+}
